5dc9798ba1ad0b12a2631e55aab03cd552645297
lgc <lgc@B024978.bom.gov.au>
2018-09-06 11:56:19 +0800

    added a new index file, homepage. css changes

--- show.php
+++ show.php
@@ -5,88 +5,100 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-         <style>
-            h2 {
-            text-align: center;
-            background-color: Lightgrey;
-            }
+<head>
+<style>
+th, td {
+	padding: 8px;
+    border-bottom: 1px solid #ddd;
+}
 
-            .btn {
-                background-color: black;
-                border: none;
-                color: white;
-                padding: 12px 16px;
-                font-size: 16px;
-                cursor: pointer;
-                    border-radius: 12px;
-            }
+tr:hover {
+    background-color:#A9CCE3;
+} 
 
-            /* Darker background on mouse-over */
-            .btn:hover {
-                background-color: orange !important;
-            }
+h2 {
+text-align: center;
+background-color: Lightgrey;
+}
 
-          </style>
-        <meta charset="UTF-8">
-        <title>Site form for the year</title>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="include.css">
-        <script type="text/javascript">
-        </script>
-    </head>
+.btn {
+    background-color: black;
+    border: none;
+    color: white;
+    padding: 12px 16px;
+    font-size: 16px;
+    cursor: pointer;
+        border-radius: 12px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+    background-color: orange !important;
+}
+
+</style>
+<meta charset="UTF-8">
+<title>Site form for the year</title>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="include.css">
+<script type="text/javascript">
+</script>
+</head>
     <body>
-    <div class="w3-container"><br>
-	<div class="w3-card-4 w3-light-grey w3-padding w3-third">
-            <h2><font color="black">Forms for the Year</font></h2>                    
-            <table id="forms_table" class="w3-table" style="width:100%">
-            <tr>
-              <th>Month</th>
-              <th>Form id</th>
-            </tr>
+    <div class="w3-row">
+        <div class="w3-container w3-third"></div>
+        <div class="w3-container w3-third"><br>
+            <div class="w3-card-4 w3-light-grey w3-padding">
+                <h2><font color="black">Forms for the Year</font></h2>                    
+                <table id="forms_table" class="w3-table" style="width:100%">
+                <tr>
+                  <th>Month</th>
+                  <th>Form id</th>
+                </tr>
 
-            <?php
-            require('piLogin.php');
-            $db = "f146";
-            $conn = mysqli_connect($host, $user, $pass, $db);
-            if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-            }
+                <?php
+                require('piLogin.php');
+                $db = "f146";
+                $conn = mysqli_connect($host, $user, $pass, $db);
+                if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                }
 
-            $year = $_POST['year'];
-            $site_num = $_POST['site_num'];
+                $year = $_POST['year'];
+                $site_num = $_POST['site_num'];
 
-            $sql = "SELECT month, form_id FROM forms WHERE site_num='" . $site_num ."' AND year='" . $year . "' ORDER BY month";
-            //echo $sql;
+                $sql = "SELECT month, form_id FROM forms WHERE site_num='" . $site_num ."' AND year='" . $year . "' ORDER BY month";
+                //echo $sql;
 
-            $result = mysqli_query($conn, $sql);
+                $result = mysqli_query($conn, $sql);
 
-            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-                    echo "<tr>";
-                    echo "<td><a href='show_order.php?form_id=" . $row['form_id'] . "' target='_parent'>" . $row['month'] . "</a></td>";
-                    echo "<td>" . $row['form_id'] . "</td>";
-                    echo "</tr>";
-            } 
-            echo "</table>";
-            /*
+                while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+                        echo "<tr>";
+                        echo "<td><a href='show_order.php?form_id=" . $row['form_id'] . "' target='_parent'>" . $row['month'] . "</a></td>";
+                        echo "<td>" . $row['form_id'] . "</td>";
+                        echo "</tr>";
+                } 
+                echo "</table>";
+                /*
 
-            $sql = "SELECT * FROM order_lines WHERE form_id='" . $form_id ."'";
-            //echo $sql;
-            $result = mysqli_query($conn, $sql);
+                $sql = "SELECT * FROM order_lines WHERE form_id='" . $form_id ."'";
+                //echo $sql;
+                $result = mysqli_query($conn, $sql);
 
-            echo "<br>Form:<br>";
-            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-                    foreach ($row as $key => $value) {
-                        echo $key . ":" . $value . " | ";
-                    }
-                    echo "<br>";
-            }
-             * 
-             */
-            mysqli_free_result($result);
-            mysqli_close($conn);
-            ?>
+                echo "<br>Form:<br>";
+                while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+                        foreach ($row as $key => $value) {
+                            echo $key . ":" . $value . " | ";
+                        }
+                        echo "<br>";
+                }
+                 * 
+                 */
+                mysqli_free_result($result);
+                mysqli_close($conn);
+                ?>
+            </div>
         </div>
     </div>
     </body>
--- show_order.php
+++ show_order.php
@@ -5,79 +5,91 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-         <style>
-            h2 {
-            text-align: center;
-            background-color: Lightgrey;
-            }
+<head>
+<style>
+th, td {
+	padding: 8px;
+    border-bottom: 1px solid #ddd;
+}
 
-            .btn {
-                background-color: black;
-                border: none;
-                color: white;
-                padding: 12px 16px;
-                font-size: 16px;
-                cursor: pointer;
-                    border-radius: 12px;
-            }
+tr:hover {
+    background-color:#A9CCE3;
+} 
 
-            /* Darker background on mouse-over */
-            .btn:hover {
-                background-color: orange !important;
-            }
+h2 {
+text-align: center;
+background-color: Lightgrey;
+}
 
-          </style>
-        <meta charset="UTF-8">
-        <title>Show Order</title>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="include.css">
-        <script type="text/javascript">
-        </script>
-    </head>
+.btn {
+    background-color: black;
+    border: none;
+    color: white;
+    padding: 12px 16px;
+    font-size: 16px;
+    cursor: pointer;
+        border-radius: 12px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+    background-color: orange !important;
+}
+
+</style>
+<meta charset="UTF-8">
+<title>Show Order</title>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="include.css">
+<script type="text/javascript">
+</script>
+</head>
     <body>
-    <div class="w3-container"><br>
-	<div class="w3-card-4 w3-light-grey w3-padding w3-third">
-            <h2><font color="black">Order Details</font></h2>                    
-            <table id="forms_table" class="w3-table" style="width:100%">
-            <tr>
-              <th>Material number</th>
-              <th>On hand previous month</th>
-              <th>Received</th>
-              <th>Consumed</th>
-              <th>Date</th>              
-            </tr>
+    <div class="w3-row">
+        <div class="w3-container w3-third"></div>
+        <div class="w3-container w3-third"><br>
+            <div class="w3-card-4 w3-light-grey w3-padding">
+                <h2><font color="black">Order Details</font></h2>                    
+                <table id="forms_table" class="w3-table" style="width:100%">
+                <tr>
+                  <th>Material number</th>
+                  <th>On hand previous month</th>
+                  <th>Received</th>
+                  <th>Consumed</th>
+                  <th>Date</th>              
+                </tr>
 
-            <?php
-            require('piLogin.php');
-            $db = "f146";
-            $conn = mysqli_connect($host, $user, $pass, $db);
-            if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-            }
+                <?php
+                require('piLogin.php');
+                $db = "f146";
+                $conn = mysqli_connect($host, $user, $pass, $db);
+                if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                }
 
-            $form_id = $_GET['form_id'];
+                $form_id = $_GET['form_id'];
 
-            $sql = "SELECT * FROM order_lines WHERE form_id='" . $form_id ."'";
-            //echo $sql;
+                $sql = "SELECT * FROM order_lines WHERE form_id='" . $form_id ."'";
+                //echo $sql;
 
-            $result = mysqli_query($conn, $sql);
+                $result = mysqli_query($conn, $sql);
 
-            while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['mat_num'] . "</td>";
-                    echo "<td width='20%'>" . $row['previous'] . "</td>";
-                    echo "<td>" . $row['rx'] . "</td>";
-                    echo "<td>" . $row['used'] . "</td>";
-                    echo "<td>" . $row['date'] . "</td>";
-                    echo "</tr>";
-            } 
-            echo "</table>";
+                while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+                        echo "<tr>";
+                        echo "<td>" . $row['mat_num'] . "</td>";
+                        echo "<td width='20%'>" . $row['previous'] . "</td>";
+                        echo "<td>" . $row['rx'] . "</td>";
+                        echo "<td>" . $row['used'] . "</td>";
+                        echo "<td>" . $row['date'] . "</td>";
+                        echo "</tr>";
+                } 
+                echo "</table>";
 
-            mysqli_free_result($result);
-            mysqli_close($conn);
-            ?>
+                mysqli_free_result($result);
+                mysqli_close($conn);
+                ?>
+            </div>
         </div>
     </div>
     </body>
